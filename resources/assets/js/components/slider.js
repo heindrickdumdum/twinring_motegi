245d3b0812e76fbe751037fb9299edb083aeed4c
Raffy Laspinas <raffy_g_laspinas@ligph.com>
2020-02-05 18:16:50 +0800

    adding slider on mobile

--- resources/assets/js/components/slider.js
+++ resources/assets/js/components/slider.js
@@ -1,11 +1,32 @@
 import Swiper from 'swiper';
 
 export default function slider() {
-  let mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 3.5,
-    spaceBetween: 50,
+  let mySwiper = new Swiper('.swiper-container', {  
     speed: 1500,
-    initialSlide: 1,
+    initialSlide: 2,
     slidesPerGroup: 3,
+    centeredSlides: false,
+
+    breakpoints: {
+        1280: {
+            slidesPerView: 3.5,
+            spaceBetween: 50,
+            loopedSlides: 0,
+            initialSlide: 1,
+        },
+        375: {
+            spaceBetween: 10,           
+            centeredSlides: true,
+            slidesPerView: 1.5,
+            initialSlide: 1,
+            loopedSlides: 0,
+            loop: true
+        }
+    }
 });
+
+mySwiper.on('resize', function () {
+    this.reInit();
+});
+
 }

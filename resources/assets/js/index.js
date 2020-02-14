e4dc44828689f9be26d54b2f24b08daaaa0014d0
Julie Pearl Verdijo <julie_pearl_verdijo@ligph.com>
2020-02-14 18:22:19 +0800

    Add slider on top page for sp

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,5 +1,14 @@
 import preload from "./components/preload";
 import topMvCarousel from "./components/top-mv-carousel";
+import slider from './components/slider';
 
 preload();
 topMvCarousel();
+
+if(window.innerWidth <= 749) {
+	slider({
+		id: 'commonSlider', // REQUIRED
+		target: '.js-slider-sp', // REQUIRED
+	})
+}
+

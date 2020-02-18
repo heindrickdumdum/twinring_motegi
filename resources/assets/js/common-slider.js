321a23e34108bab410d655de751d14c8e5708a46
Julie Pearl Verdijo <julie_pearl_verdijo@ligph.com>
2020-02-18 18:18:59 +0800

    Fix pixel perfect on craft page pc

--- resources/assets/js/common-slider.js
+++ resources/assets/js/common-slider.js
@@ -11,3 +11,22 @@ slider({
   id: 'commonSliderBottom', // REQUIRED
   target: '.js-slider-bottom', // REQUIRED
 })
+
+slider({
+  id: 'commonSlider', // REQUIRED
+  target: '.js-slider-craft', // REQUIRED
+  options: {
+		slidesPerView: 3,
+		spaceBetween: 60,
+		breakpoints: {
+			1280: {
+				slidesPerView: 3,
+				spaceBetween: 60,
+			},
+      375: {
+        spaceBetween: 17,
+        slidesPerView: 3,
+      }
+    },
+	}
+})

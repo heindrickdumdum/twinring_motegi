999697f7d31ea1150cd22943f08e0af38ce773b0
Angelo Abella <angelo_abella@ligph.com>
2020-02-20 16:46:18 +0800

    updated forest lab page (text and images)

---
+++ resources/assets/js/components/banner-fade.js
@@ -0,0 +1,22 @@
+export default function bannerFade(params) {
+  const items = document.querySelectorAll(params.item);
+  const firstItem = items[0];
+
+  setTimeout(() => {
+    let currentIdx = 0;
+    firstItem.classList.add('is-active');
+
+    setInterval(() => {
+      items.forEach((item) => item.classList.remove('is-active'));
+      firstItem.classList.remove('is-active');
+
+      currentIdx++;
+
+      if(currentIdx >= items.length) {
+        currentIdx = 0;
+      }
+
+      items[currentIdx].classList.add('is-active');
+    }, 4200);
+  }, 4200);
+}

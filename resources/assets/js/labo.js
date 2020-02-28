c918be2ccc9272b349677fc9af94e430423ec686
Lovely Jaca <lovely_jaca@ligph.com>
2020-02-28 16:22:26 +0800

    fb updates

--- resources/assets/js/labo.js
+++ resources/assets/js/labo.js
@@ -1,5 +1,54 @@
-import bannerFade from './components/banner-fade.js';
+// import bannerFade from './components/banner-fade.js';
+//
+// bannerFade({
+//   item: '.js-banner-item'
+// });
 
-bannerFade({
-  item: '.js-banner-item'
-});
+import Swiper from 'swiper/dist/js/swiper.js';
+
+export default class galleryLabo {
+  constructor() {
+
+    let galleryThumb = new Swiper('.js-gallery-thumb', {
+      spaceBetween: 10,
+      variableWidth: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    })
+
+    new Swiper('.js-gallery', {
+      speed: 2000,
+      slidesPerView: 1,
+      fadeEffect: { crossFade: true },
+      effect: 'fade',
+      thumbs: {
+        swiper: galleryThumb
+      }
+    })
+
+    let galleryThumb2 = new Swiper('.js-gallery-thumb2', {
+      spaceBetween: 10,
+      variableWidth: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    })
+
+    new Swiper('.js-gallery2', {
+      speed: 2000,
+      slidesPerView: 1,
+      fadeEffect: { crossFade: true },
+      effect: 'fade',
+      thumbs: {
+        swiper: galleryThumb2
+      }
+    })
+  }
+}
+
+new galleryLabo();

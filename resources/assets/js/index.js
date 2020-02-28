b7d0df9657120bbac0582aaae34a6ddd5f9fe7c6
Lovely Jaca <lovely_jaca@ligph.com>
2020-02-28 17:41:42 +0800

    slider adjustments

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -8,6 +8,38 @@ topMvCarousel();
 topFeatureSlider({
   id: 'topFeature', // REQUIRED
   target: '.js-feature-slider', // REQUIRED
+  options: {
+    speed: 1500,
+    slidesPerView: 'auto',
+    variableWidth: 'auto',
+    spaceBetween: 55,
+    loop: true,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+
+    breakpoints: {
+      1280: {
+        slidesPerGroup: 3
+      },
+      375: {
+        spaceBetween: 10,
+        slidesPerGroup: 1
+      }
+    },
+
+    on: {
+      resize: function() {
+        this.params.observer = true;
+      }
+    }
+  }
 })
 
 topFeatureSlider({

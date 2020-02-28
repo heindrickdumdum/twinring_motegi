cab27b946be2b5d09e8d922c2b7f0ca283bda26f
Lovely Jaca <lovely_jaca@ligph.com>
2020-02-28 11:36:02 +0800

    update pickup

--- resources/assets/js/common-slider.js
+++ resources/assets/js/common-slider.js
@@ -26,17 +26,17 @@ slider({
         pagination: {
           el: '.swiper-pagination',
           clickable: true
-        } 
+        }
       }
     }
   }
 })
 
 // SUMIKA , ITADAKI, AND DOKIDOKI BOTTOM SLIDER
-slider({
-  id: 'commonSliderBottom', // REQUIRED
-  target: '.js-slider-bottom', // REQUIRED
-})
+// slider({
+//   id: 'commonSliderBottom', // REQUIRED
+//   target: '.js-slider-bottom', // REQUIRED
+// })
 
 // slider({
 //   id: 'commonSlider', // REQUIRED
@@ -56,3 +56,26 @@ slider({
 //     },
 // 	}
 // })
+
+slider({
+  id: 'commonSliderBottom', // REQUIRED
+  target: '.js-slider-bottom', // REQUIRED
+  options: {
+    slidesPerView: 'auto',
+    variableWidth: true,
+
+    breakpoints: {
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 60,
+        centeredSlides: false
+
+      },
+      375: {
+        spaceBetween: 25,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+      }
+    }
+  }
+})

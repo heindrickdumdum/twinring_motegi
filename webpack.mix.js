0f0ab58aad0bba3812c3ab49fad7e4f7de4f9754
Regene Vilvestre <regene_vilvestre@ligph.com>
2020-03-03 11:54:00 +0800

    remove test page copywatch

--- webpack.mix.js
+++ webpack.mix.js
@@ -104,11 +104,6 @@ mix
     `${distRelativePath}/assets/lib/fonts`,
     { base: `${srcRelativePath}/assets/lib/fonts` }
 	)
-	.copyWatched(
-    `${srcRelativePath}/assets/lib/test`,
-    `${distRelativePath}/assets/lib/test`,
-    { base: `${srcRelativePath}/assets/lib/test` }
-  )
   .browserSync({
     open: true,
     host: process.env.MIX_BROWSER_SYNC_HOST || 'localhost',

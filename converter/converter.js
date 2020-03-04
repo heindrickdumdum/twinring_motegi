a06baca2d3c3d3d9a2b494f55a204cd0cf3d4277
zwatanabe <zenta.watanabe@zelkworks.com>
2020-03-04 18:26:02 +0900

    merge converter

---
+++ converter/converter.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var main  = require('./main.js');
+var path = require('path');
+
+function eachFiles(filepath, rootPath, callback) {
+
+    if (!rootPath) {
+        rootPath = filepath;
+    }
+    var stat = fs.statSync(filepath);
+    if (!stat) {
+
+    } else if (stat.isDirectory()) {
+        try {
+            var files = fs.readdirSync(filepath, '');
+
+            if (!files) {
+
+            } else {
+                for (var _i in files)(function (i) {
+                    var file = files[i];
+                    
+                    if (filepath.match(/.*\/$/)) {
+                        eachFiles(filepath + file, rootPath, callback);
+                    } else {
+                        eachFiles(filepath + "/" + file, rootPath, callback);
+                    }
+                }(_i));
+            }
+        } catch (e1) {
+            console.error("Directory " + filepath + " is unreadable.");
+        }
+    } else if (stat.isFile()) {
+        if (callback) {
+            callback.call(this, filepath, rootPath);
+        }
+    } else {
+        console.error(filepath + " is not file or directory");
+    }
+}
+
+//Try only all of .html file
+eachFiles(__dirname + '/../public', null, function(filePath, rootPath) {
+	var targetFileName = path.basename(filePath);
+	if(path.basename(targetFileName).indexOf('.html') !== -1){
+		main.mtgConvert(filePath);
+		// Test code
+		// main.mtgConvert(__dirname + '/../public/index.html');
+	}
+});
---
+++ converter/main.js
@@ -0,0 +1,32 @@
+var fs = require('fs'); // fs module
+var iconv = require('iconv-lite'); // support Shift-jis
+
+exports.mtgConvert = function (targetFile) {
+	
+	// Get html
+	var htmlTags = fs.readFileSync(targetFile);
+	var htmlTagsSJ = iconv.decode(htmlTags, 'Shift_JIS');
+
+	// console.log(typeof htmlTagsSJ);
+	var startLine = htmlTagsSJ.indexOf('</header>');
+	var endLine = htmlTagsSJ.indexOf('<footer class="footer">');
+
+	var HEADER_TAG_LENGTH = 9;
+
+	// htmlTagsSJ.slice(startLine)
+	var contentsTags = htmlTagsSJ.slice(startLine + HEADER_TAG_LENGTH, endLine)
+	// console.log(contentsTags);
+
+	//Update html files
+	function writeFile(path, data) {
+	  fs.writeFile(path, data, function (err) {
+	    if (err) {
+	        throw err;
+	    }
+	  });
+	}
+	writeFile(targetFile, contentsTags);
+
+	console.log('success: ' + targetFile);
+
+};

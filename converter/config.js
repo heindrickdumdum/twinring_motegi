ba832f5e75c8901718ad414a45525aa04975d497
Seito <horiguchi_seito@liginc.co.jp>
2020-03-04 20:20:19 +0900

    copy assets function

--- converter/config.js
+++ converter/config.js
@@ -2,18 +2,20 @@ module.exports = {
 	dist: __dirname + '/../public',
 	release: __dirname + '/../release',
 	assets: __dirname + '/../public/assets',
-	array1: [
-		'/dokidoki',
-		'/globalnavi',
-		'/hellowoods',
-		'/itadaki',
-		'/mobipark_m',
-		'/sumika',
-		'/top'
-	],
-	array2: [
-		'/hellowoods/program',
-		'/hellowoods/zip-line',
-		'/mobipark_m/attractions'
+	directories: [
+		[
+			'/dokidoki',
+			'/globalnavi',
+			'/hellowoods',
+			'/itadaki',
+			'/mobipark_m',
+			'/sumika',
+			'/top'
+		],
+		[
+			'/hellowoods/program',
+			'/hellowoods/zip-line',
+			'/mobipark_m/attractions'
+		]
 	]
 }
--- converter/fs.js
+++ converter/fs.js
@@ -6,4 +6,18 @@ var CONFIG = require('./config.js'); // fs module
 
 fs.copySync(CONFIG.dist, CONFIG.release);
 
-// fs.copySync(CONFIG.assets, );
+var directorySecond =CONFIG.directories[0];
+// var directoryThird =CONFIG.directories[1];
+
+//Clone assets directory to each 2nd directory
+var cloneAssets = function(target){
+	var index = 0;
+	while (index < target.length) {
+		var eachAssetsDirectory = CONFIG.release + target[index] + '/assets';
+		fs.mkdirsSync(eachAssetsDirectory);
+		fs.copySync(CONFIG.assets, eachAssetsDirectory);
+		index++;
+	}
+};
+
+cloneAssets(directorySecond);

c9c7c9c9fb5291c7337012a18b3b19eb666446f6
seito <seitokeri03@yahoo.co.jp>
2020-03-09 17:53:10 +0900

    confirmed to work

--- converter/arrangePath.js
+++ converter/arrangePath.js
@@ -1,6 +1,7 @@
 var fs = require('fs-extra');
 var replace = require('replace');
 var CONFIG = require('./config.js');
+var fn = require('./function.js');
 
 module.exports = function(){
 
@@ -9,6 +10,16 @@ module.exports = function(){
 	replace({
 	  regex: "foo",
 	  replacement: "bar",
-	  paths: CONFIG.release + '/index.html'
+	  paths: __dirname + '/../release/index.html'
 	});
+
+	// fn.eachFiles(CONFIG.dist, null, function(filePath, rootPath) {
+	// 	var targetFileName = path.basename(filePath);
+	// 	//Target only .html file
+	// 	if(path.basename(targetFileName).indexOf('.html') !== -1){
+	// 		fn.optimizeHtml(filePath);
+	// 		// Test code
+	// 		// fn.optimizeHtml(__dirname + '/../release/index.html');
+	// 	}
+	// });
 };
--- converter/main.js
+++ converter/main.js
@@ -7,43 +7,43 @@ var fn = require('./function.js');
 var init = require('./init.js');
 var arrangePath = require('./arrangePath.js');
 
+function convertHtml(){
+	var header = {
+		type: {
+			utf8: CONFIG.release + CONFIG.commonParts.directory + CONFIG.commonParts.files[0],
+			shiftjis: CONFIG.release + CONFIG.commonParts.directory + CONFIG.commonParts.files[1]
+		},
+		startTag: CONFIG.tag.header.start,
+		endTag: CONFIG.tag.header.end,
+		endTagLen: CONFIG.tag.header.end.length
+	};
+	var footer = {
+		type: {
+			utf8: CONFIG.release + CONFIG.commonParts.directory + CONFIG.commonParts.files[2],
+			shiftjis: CONFIG.release + CONFIG.commonParts.directory + CONFIG.commonParts.files[3]
+		},
+		startTag: CONFIG.tag.footer.start,
+		endTag: CONFIG.tag.footer.end,
+		endTagLen: CONFIG.tag.footer.end.length
+	};
+
+	// Watch all directories and convert all files
+	fn.eachFiles(CONFIG.release, null, function(filePath, rootPath) {
+		var targetFileName = path.basename(filePath);
+		//Target only .html file
+		if(path.basename(targetFileName).indexOf('.html') !== -1){
+			fn.optimizeHtml(filePath);
+			// Test code
+			// fn.optimizeHtml(__dirname + '/../release/index.html');
+		}
+	});
+
+	fn.optimizeHtmlParts(header.type.utf8, header.startTag, header.endTag, header.endTagLen, true);
+	fn.optimizeHtmlParts(header.type.shiftjis, header.startTag, header.endTag, header.endTagLen, false);
+	fn.optimizeHtmlParts(footer.type.utf8, footer.startTag, footer.endTag, footer.endTagLen, true);
+	fn.optimizeHtmlParts(footer.type.shiftjis, footer.startTag, footer.endTag, footer.endTagLen, false);
+}
+
 init();
-arrangePath();
-
-var header = {
-	type: {
-		utf8: CONFIG.release + CONFIG.commonParts.directory + CONFIG.commonParts.files[0],
-		shiftjis: CONFIG.release + CONFIG.commonParts.directory + CONFIG.commonParts.files[1]
-	},
-	startTag: CONFIG.tag.header.start,
-	endTag: CONFIG.tag.header.end,
-	endTagLen: CONFIG.tag.header.end.length
-};
-var footer = {
-	type: {
-		utf8: CONFIG.release + CONFIG.commonParts.directory + CONFIG.commonParts.files[2],
-		shiftjis: CONFIG.release + CONFIG.commonParts.directory + CONFIG.commonParts.files[3]
-	},
-	startTag: CONFIG.tag.footer.start,
-	endTag: CONFIG.tag.footer.end,
-	endTagLen: CONFIG.tag.footer.end.length
-};
-
-// Watch all directories and convert all files
-fn.eachFiles(CONFIG.dist, null, function(filePath, rootPath) {
-	var targetFileName = path.basename(filePath);
-	//Target only .html file
-	if(path.basename(targetFileName).indexOf('.html') !== -1){
-		fn.optimizeHtml(filePath);
-		// Test code
-		// fn.optimizeHtml(__dirname + '/../release/index.html');
-	}
-});
-
-
-// fn.optimizeHtmlParts(header.type.utf8, header.startTag, header.endTag, header.endTagLen, true);
-// fn.optimizeHtmlParts(header.type.shiftjis, header.startTag, header.endTag, header.endTagLen, false);
-// fn.optimizeHtmlParts(footer.type.utf8, footer.startTag, footer.endTag, footer.endTagLen, true);
-// fn.optimizeHtmlParts(footer.type.shiftjis, footer.startTag, footer.endTag, footer.endTagLen, false);
-
-
+convertHtml();
+// arrangePath();

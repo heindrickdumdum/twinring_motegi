7c2790a01bc58794b3f0cfb187a803e0015e9536
seito <seitokeri03@yahoo.co.jp>
2020-03-10 02:27:12 +0900

    get depth

--- converter/arrangePath.js
+++ converter/arrangePath.js
@@ -1,6 +1,8 @@
 var fs = require('fs-extra');
 var replace = require('replace');
 var path = require('path');
+var depth = require('depth');
+var rootPath = require('app-root-path');
 var CONFIG = require('./config.js');
 var fn = require('./function.js');
 
@@ -17,41 +19,76 @@ module.exports = function(){
 		var index = 0;
 		var len = array.length;
 
-		//Go to each directory
-		while (index < len) {
+		// //Go to each directory
+		// while (index < len) {
 
-			//Go to each file in the directory
-			fn.eachFiles(CONFIG.release + array[index], null, function(filePath, rootPath) {
-
-				console.log('filePath', filePath);
+		// 	//Go to each file in the directory
+		// 	fn.eachFiles(CONFIG.release + array[index], null, function(filePath, rootPath) {
 				
-				var targetFileName = path.basename(filePath);
+		// 		var targetFileName = path.basename(filePath);
 				
-				//Target only .html file
-				if(path.basename(targetFileName).indexOf('.html') !== -1){
-					// Test code
-					// fn.optimizeHtml(__dirname + '/../release/index.html');
-					// console.log(replace);
-					// replaced target is 'assets/'
-					replace({
-					  regex: 'assets/',
-					  replacement: '../assets/',
-					  paths: __dirname + '/../release/index.html'
-					});
+		// 		//Target only .html file
+		// 		if(targetFileName.indexOf('.html') !== -1){
+		// 			// console.log('hi1', filePath);
+		// 			// Test code
+		// 			// fn.optimizeHtml(__dirname + '/../release/index.html');
+		// 			// console.log(replace);
+		// 			// replaced target is 'assets/'
+		// 			replace({
+		// 			  regex: 'assets/',
+		// 			  replacement: '../assets/',
+		// 			  paths: filePath
+		// 			});
+		// 			// console.log('hi2');
 
-					// replaced target is '/assets/'
-					replace({
-					  regex: '/assets/',
-					  replacement: '../assets/',
-					  paths: __dirname + '/../release/index.html'
-					});
+		// 			// replaced target is '/assets/'
+		// 			replace({
+		// 			  regex: '/assets/',
+		// 			  replacement: '../assets/',
+		// 			  paths: filePath
+		// 			});
+		// 			// console.log('hi3');
 
-					// console.log(array[index]);
-				}
-			});
+		// 			// console.log(array[index]);
+		// 		}
+		// 	});
 			
-			index += 1;
-		}	
+		// 	index += 1;
+		// }
+			
+
+		fn.eachFiles(CONFIG.release, null, function(filePath, rootPath) {
+			var targetFileName = path.basename(filePath);
+			//Target only .html file
+			if(targetFileName.indexOf('.html') !== -1){
+				
+
+				var thisPath = path.normalize(filePath);
+				var thisRootPath = path.normalize(rootPath);
+
+				var thisSimplePath = thisPath.replace(thisRootPath, '');
+
+				//get depth -> 0, 1, 2 ...
+				var thisDepth = depth(thisSimplePath);
+				console.log(thisDepth);
+
+				// console.log(normalPath.replace(normalRootPath, ''));
+
+				// console.log(normalRootPath);
+
+				// console.log(path.normalize(filePath));
+				// console.log(path.dirname(targetFileName));
+				
+				
+				// replace({
+				// 	regex: 'assets/',
+				// 	replacement: '../assets/',
+				// 	paths: filePath
+				// });
+				// Test code
+				// fn.optimizeHtml(__dirname + '/../release/index.html');
+			}
+		});
 	}
 
 	replaceAllPath(directoryThird);

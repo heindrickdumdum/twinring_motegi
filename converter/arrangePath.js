868894f73c614489ea5a0926b5c86e0fd0d159c4
seito <seitokeri03@yahoo.co.jp>
2020-03-10 13:25:50 +0800

    removed unnecessary code

--- converter/arrangePath.js
+++ converter/arrangePath.js
@@ -1,5 +1,4 @@
 var fs = require('fs-extra');
-// var replace = require('replace');
 var path = require('path');
 var depth = require('depth');
 var rootPath = require('app-root-path');
@@ -11,47 +10,6 @@ module.exports = function(){
 
 	var directorySecond = CONFIG.directories[0];
 	var directoryThird = CONFIG.directories[1];
-	
-	// console.log('directorySecond', directorySecond);
-	// CONFIG.directories[1]
-
-
-		// //Go to each directory
-		// while (index < len) {
-
-		// 	//Go to each file in the directory
-		// 	fn.eachFiles(CONFIG.release + array[index], null, function(filePath, rootPath) {
-				
-		// 		var targetFileName = path.basename(filePath);
-				
-		// 		//Target only .html file
-		// 		if(targetFileName.indexOf('.html') !== -1){
-		// 			// console.log('hi1', filePath);
-		// 			// Test code
-		// 			// fn.optimizeHtml(__dirname + '/../release/index.html');
-		// 			// console.log(replace);
-		// 			// replaced target is 'assets/'
-		// 			replace({
-		// 			  regex: 'assets/',
-		// 			  replacement: '../assets/',
-		// 			  paths: filePath
-		// 			});
-		// 			// console.log('hi2');
-
-		// 			// replaced target is '/assets/'
-		// 			replace({
-		// 			  regex: '/assets/',
-		// 			  replacement: '../assets/',
-		// 			  paths: filePath
-		// 			});
-		// 			// console.log('hi3');
-
-		// 			// console.log(array[index]);
-		// 		}
-		// 	});
-			
-		// 	index += 1;
-		// }
 	var relativePath = function(selfDepth){
 		if(selfDepth === 0 || selfDepth === 1){
 			return './assets';

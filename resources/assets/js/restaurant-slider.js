db0e9af50b4adacf661f5a7c98951a9589a3b052
Angelo Abella <angelo_abella@ligph.com>
2020-04-03 13:52:10 +0800

    additional updates

--- resources/assets/js/restaurant-slider.js
+++ resources/assets/js/restaurant-slider.js
@@ -1,55 +1,32 @@
 import slider from './components/slider';
 
-let options = {
-  fadeEffect: { 
-    crossFade: true 
-  },
-  virtualTranslate: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false
-  },
-  speed: 1000,
-  slidesPerView: 1,
-  effect: 'fade',
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
+slider({//restaurant page
+  id: 'restaurantSlider',
+  target: '.js-slider-restaurant-marcherant, .js-slider-restaurant-toya, .js-slider-restaurant-oak, .js-slider-restaurant-granturismo, .js-slider-restaurant-forestkitchen, .js-slider-restaurant-rocco',
+  options: {
+    autoplay: {
+      delay: 2500,
+      disableOnIteraction: true
+    },
+    speed: 1000,
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    breakpoints: {
+      749: {
+        autoplay: {
+          delay: 2500,
+          disableOnIteraction: true
+        },
+        speed: 1000,
+        slidesPerView: 1,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
+      }
+    }
   }
-}
-
-slider({//restaurant page
-  id: 'restaurantMarcherantSlider',
-  target: '.js-slider-restaurant-marcherant',
-  options: options
-});
-
-slider({//restaurant page
-  id: 'restaurantToyaSlider',
-  target: '.js-slider-restaurant-toya',
-  options: options
-});
-
-slider({//restaurant page
-  id: 'restaurantOakSlider',
-  target: '.js-slider-restaurant-oak',
-  options: options
-});
-
-slider({//restaurant page
-  id: 'restaurantGranturismoSlider',
-  target: '.js-slider-restaurant-granturismo',
-  options: options
-});
-
-slider({//restaurant page
-  id: 'restaurantForestkitchenSlider',
-  target: '.js-slider-restaurant-forestkitchen',
-  options: options
-});
-
-slider({//restaurant page
-  id: 'restaurantRoccoSlider',
-  target: '.js-slider-restaurant-rocco',
-  options: options
 });

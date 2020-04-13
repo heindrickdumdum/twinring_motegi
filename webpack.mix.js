5f2181694356f62c2768d852c5086f7f32e0158a
Heindrick Dumdum <heindrick_dumdum@ligph.com>
2020-04-13 18:38:26 +0800

    Program Details PC

--- webpack.mix.js
+++ webpack.mix.js
@@ -76,6 +76,11 @@ mix
     `${distRelativePath}/assets/js`,
   )
 
+  .js(
+    `${srcRelativePath}/assets/js/program-details.js`,
+    `${distRelativePath}/assets/js`,
+  )
+
   .eslint()
   .sass(
     `${srcRelativePath}/assets/css/header.scss`,

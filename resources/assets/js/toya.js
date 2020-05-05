55f7060c8e1528d3318befbe78e50dcef662f77b
Angelo Abella <angelo_abella@ligph.com>
2020-05-05 16:57:06 +0800

    initial creation of page PC

--- resources/assets/js/toya.js
+++ resources/assets/js/toya.js
@@ -4,7 +4,7 @@ tabContent = document.querySelectorAll('.js-tab-content');
 tabLink = document.querySelectorAll('.js-tab-link');
 
 tabContent.forEach(item => {
-  item.style.display = "none";
+  item.style.height = "0px";
   item.style.opacity = 0;
 });
 
@@ -12,7 +12,7 @@ tabLink.forEach(item => {
   if( item.classList.contains('is-active') ) {
     let thisId = item.getAttribute('data-tab');
 
-    document.getElementById(thisId).style.display = "block";
+    document.getElementById(thisId).style.height = "auto";
     document.getElementById(thisId).style.opacity = 1;
   }
 
@@ -26,11 +26,26 @@ tabLink.forEach(item => {
     let tabId = item.getAttribute('data-tab');
 
     for(j=0; j<tabContent.length; j++) {
-      tabContent[j].style.display = "none";
+      tabContent[j].style.height = "0px";
       tabContent[j].style.opacity = 0;
     }
-
-    document.getElementById(tabId).style.display = "block";
+    
+    document.getElementById(tabId).style.height = "auto";
     document.getElementById(tabId).style.opacity = 1;
   });
 } );
+
+import slider from './components/slider';
+
+slider({
+  id: 'sliderGallery',
+  target: '.js-spring-gallery, .js-summer-gallery, .js-autumn-gallery, .js-winter-gallery',
+  options: {
+    speed: 1500,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  }
+});

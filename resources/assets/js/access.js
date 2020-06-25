0f71367f35ee05d9fe58a03190fc9d6c01bd848a
Angelo Abella <angelo_abella@ligph.com>
2020-06-25 09:38:50 +0800

    sp adjustments

--- resources/assets/js/access.js
+++ resources/assets/js/access.js
@@ -47,3 +47,55 @@ accessTab('#js-train-tab');
 
 //bus tab
 accessTab('#js-bus-tab');
+
+import Swiper from 'swiper/dist/js/swiper.js';
+
+export default class SliderCarBike {
+  constructor() {
+    // breakpoint where swiper will be destroyed
+    // and switches to a dual-column layout
+    const breakpoint = window.matchMedia( '(min-width:750px)' );
+
+    let swiperCarbike;
+
+    const breakpointChecker = function() {
+
+      if ( breakpoint.matches === true ) {
+
+        // if(!swiperCarbike) return;
+        if (swiperCarbike !== undefined ) {
+          swiperCarbike.destroy( true, true );
+        }
+
+        return;
+      } else if ( breakpoint.matches === false ) {
+        return enableSwiper();
+      }
+    };
+
+    const enableSwiper = function() {
+
+      swiperCarbike = new Swiper ('.js-slider-tab', {
+
+        // loop: true,
+        slidesPerView: 'auto',
+        variableWidth: true,
+        centeredSlides: false,
+        spaceBetween: 0,
+        keyboardControl: true,
+        grabCursor: true,
+      });
+
+    };
+
+
+    // keep an eye on viewport size changes
+    breakpoint.addListener(breakpointChecker);
+
+    // kickstart
+    breakpointChecker();
+
+  }
+}
+
+new SliderCarBike();

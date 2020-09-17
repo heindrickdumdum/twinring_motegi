3638ac3695d9b0669a63bb9f8460c2d2438aa1c0
Rinnah <rinnah_lagroma@ligph.com>
2020-09-17 17:01:38 +0800

    updated

--- resources/assets/js/read-more.js
+++ resources/assets/js/read-more.js
@@ -1,12 +1,13 @@
 import $ from 'jquery';
 
-$('.moreless-button .member-pass-span').click(function() {
+$('.moreless-button').click(function() {
   $('.moretext').slideToggle();
-  if ($('.moreless-button .member-pass-span').text() == "その他全７つのお得な特典と入会方法") {
-    $(this).addClass("is-open");
-    $(this).text("閉じる");
+  if ($('.moreless-button').html() == '<span>その他全７つのお得な特典と入会方法</span>') {
+
+    $(this).html("<span>閉じる</span>");
+    $('.moreless-button span').addClass("is-open");
   } else {
-    $(this).text("その他全７つのお得な特典と入会方法");
-    $(this).removeClass("is-open");
+    $(this).html("<span>その他全７つのお得な特典と入会方法</span>");
+    $('.moreless-button span').removeClass("is-open");
   }
 });

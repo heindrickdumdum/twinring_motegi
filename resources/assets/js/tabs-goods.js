4b17429949b6c7102c471a0fe727981685306fa1
Rinnah <rinnah_lagroma@ligph.com>
2020-10-05 18:21:17 +0800

    fixes

--- resources/assets/js/tabs-goods.js
+++ resources/assets/js/tabs-goods.js
@@ -67,18 +67,3 @@ window.onload = () => {
 
   }
 }
-
-const tabSelect = document.querySelector('.custom-select-goods-sp');
-const main = document.querySelector('.bg-grey');
-let navOffsetop = tabSelect.getBoundingClientRect().top;
-
-window.addEventListener('scroll', function(){
-  if (navOffsetop <= window.pageYOffset) {
-    tabSelect.classList.add('is-fixed');
-    main.style.marginTop = '95px';
-  } else {
-    tabSelect.classList.remove('is-fixed');
-    main.style.marginTop = '0';
-  }
-
-});
